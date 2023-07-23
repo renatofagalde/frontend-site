03c1e17c15c1cbb2eb2ee84853530a73e2a91352
renato@likwi.com.br <renato@likwi.com.br>
2023-07-23 20:12:10 -0300

    estrutura do site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import {Component} from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dynamic-website';
+    title = 'Estrutura padrão do site';
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+    constructor(private primengConfig: PrimeNGConfig) {
+    }
 
-  ngOnInit() {
-    this.primengConfig.zIndex = {
-      modal: 1100,    // dialog, sidebar
-      overlay: 1000,  // dropdown, overlaypanel
-      menu: 1000,     // overlay menus
-      tooltip: 1100   // tooltip
-    };
-  }
+    ngOnInit() {
+        this.primengConfig.zIndex = {
+            modal: 1100,    // dialog, sidebar
+            overlay: 1000,  // dropdown, overlaypanel
+            menu: 1000,     // overlay menus
+            tooltip: 1100   // tooltip
+        };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ButtonModule} from "primeng/button";
 import {CheckboxModule} from "primeng/checkbox";
 import {PanelModule} from "primeng/panel";
+import {StyleClassModule} from "primeng/styleclass";
+import {RippleModule} from "primeng/ripple";
+import {AvatarModule} from "primeng/avatar";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ButtonModule,
-    CheckboxModule,
-    PanelModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        StyleClassModule,
+        RippleModule,
+        ButtonModule,
+        CheckboxModule,
+        PanelModule,
+        AvatarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
